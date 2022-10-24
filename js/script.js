f75296305ba8a02501034ea5600d16c34edea3a8
Sebastian Kowalski <wwy.skowalski@wp.pl>
2022-10-24 20:49:48 +0200

    Add else if

--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+//Variables
+var computerMove, playerMove, randomNumber;
+
+randomNumber = Math.floot(Math.random() * 3 + 1);
+console.log("Twoja wylosowana liczba to:" + randomNumber);
+if(randomNumber == '1') {
+    computerMove = 'kamień';
+} else if(randomNumber == '2') {
+    computerMove = "papier";
+} else if(randomNumber=="3") {
+    computerMove = 'nozyce';
+} else {
+    computerMove = "nieznany ruch";
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-//cwiczenie 1
-var computerMove;
-var playerMove;
-computerMove = "Kamień";
-playerMove = "Papier";
-printMessage('Zagrałem' + computeMove + 'Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem' + playerMove + 'Jeśli Twój ruch to nożyce, to wygrywasz!');
-
-//cwiczenie 2
-var randomNumber;
-randomNumber = Math.floor(Math.random() * 8 + 11);
-printMessage('Wylosowana liczba to: ' + randomNumber);
+printMessage('Mój ruch: ' + computerMove);
