a8a848ef7319d2d9206bc24a8d02143476624c7b
Sebastian Kowalski <wwy.skowalski@wp.pl>
2022-11-05 11:13:26 +0100

    Add funcions sidplayResult and getMoveName

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
 //Variables
-var computerMove, playerMove, randomNumber, playerInput;
+var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
 
-randomNumber = Math.floot(Math.random() * 3 + 1);
-console.log("Twoja wylosowana liczba to:" + randomNumber);
-if(randomNumber == '1') {
-    computerMove = 'kamień';
-} else if(randomNumber == '2') {
-    computerMove = "papier";
-} else if(randomNumber=="3") {
-    computerMove = 'nozyce';
-} else {
-    computerMove = "nieznany ruch";
+function getMoveName(argMoveId) {
+  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+  if (argMoveId == 1) {
+    return 'kamień';
+  } else if (argMoveId == 2){
+    return 'papier';
+  } else if(argMoveId == 3){
+    return 'nożyce';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+    return 'kamień';
+  }
 }
 
-printMessage('Mój ruch: ' + computerMove);
-
+function displayResult(argPlayerMove, argComputerMove) {
+  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+    printMessage('Wygrywasz!');
+  } else if(argPlayerMove == 'kamień' && argComputerMove == 'nożyce'){
+    printMessage('Wygrywasz!');
+  } else if(argPlayerMove == 'nożyce' && argComputerMove == 'papier'){
+    printMessage('Wygrywasz!');
+  } else if(argPlayerMove == argComputerMove){
+    printMessage('Remis!');
+  } else {
+    printMessage('Przegrywasz :(');
+  }
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+}
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-if(playerInput == '1') {
-    playerMove = 'kamień';
-} else if(playerInput == '2') {
-    playerMove = "papier";
-} else if(playerInput == "3") {
-    playerMove = 'nozyce';
-} else {
-    playerMove = "podaj calkowita wartosc z zakresu <1,3>";
-}
-
-printMessage('Twój ruch: ' + computerMove);
+console.log('wybór ruchu gracza to: ' + playerInput);
+playerMove = getMoveName(playerInput);
+console.log('ruch gracza to: ' + playerMove);
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+computerMove = getMoveName(randomNumber);
+console.log('ruch komputera to: ' + computerMove);
+displayResult(playerMove, computerMove);
